8de5f23d6d2840f95a153da3c8d99020b7b759f7
ThomasGuy <twguy66@gmail.com>
2026-04-18 06:28:21 +0700

    Mapped sqlalchemy column

--- src/database/models/user.py
+++ src/database/models/user.py
@@ -3,7 +3,7 @@ from datetime import datetime, timezone
 
 from sqlalchemy import Column
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql.sqltypes import Boolean, DateTime, String
 
 from src.database import Base
@@ -12,10 +12,12 @@ from src.database import Base
 class User(Base):
     __tablename__ = "user"
 
-    id = Column(
-        UUID(as_uuid=True), primary_key=True, nullable=False, index=True, default=uuid.uuid4
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4
     )
-    public_id = Column(UUID(as_uuid=True), unique=True, nullable=False, default=uuid.uuid4)
+    public_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), unique=True, nullable=False, default=uuid.uuid4
+    )
     username = Column(String, nullable=False, unique=True)
     email = Column(String, nullable=False, unique=True)
     registered_on = Column(DateTime, default=datetime.now(timezone.utc))
